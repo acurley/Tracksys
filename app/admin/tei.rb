ea27ed4872ef9a24794992de77cfe602d6d73f77
Matthew Stephens <mattbastard@gmail.com>
2012-11-26 11:25:45 -0500

    adding button for fedora ingest

--- app/admin/tei.rb
+++ app/admin/tei.rb
@@ -53,4 +53,26 @@ ActiveAdmin.register Tei do
 
   end
 
+
+  sidebar "Digital Library Workflow", :only => [:show] do 
+    if tei.valid?
+      div :class => 'workflow_button' do button_to "Put into Digital Library", start_ingest_from_archive_admin_unit_path(:datastream => 'all'), :method => :put end
+    end
+    if tei.in_dl?
+      div :class => 'workflow_button' do button_to "Update All Datastreams", update_metadata_admin_unit_path(:datastream => 'all'), :method => :put end
+      div :class => 'workflow_button' do button_to "Update All XML Datastreams", update_metadata_admin_unit_path(:datastream => 'allxml'), :method => :put end
+      div :class => 'workflow_button' do button_to "Update Dublin Core", update_metadata_admin_unit_path(:datastream => 'dc_metadata'), :method => :put end
+      div :class => 'workflow_button' do button_to "Update Descriptive Metadata", update_metadata_admin_unit_path(:datastream => 'desc_metadata'), :method => :put end
+      div :class => 'workflow_button' do button_to "Update Relationships", update_metadata_admin_unit_path(:datastream => 'rels_ext'), :method => :put end
+      div :class => 'workflow_button' do button_to "Update Index Records", update_metadata_admin_unit_path(:datastream => 'solr_doc'), :method => :put end
+    end
+  end
+
+ # Member actions for workflow
+  member_action :start_ingest_from_archive, :method => :put do
+  end
+
+  member_action :update_metadata, :method => :put do 
+  end
+
 end
