5dce3ba92833cb737ca371cb3547ae541bbf3eb6
Andrew Curley <andrew.curley@gmail.com>
2013-04-12 19:20:06 -0400

    Add more specs

--- spec/factories/master_files.rb
+++ spec/factories/master_files.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :master_file do
+    filename '123.tif'
+    filesize '100'
+    title 'Test image'
   end
 end
--- spec/models/master_file_spec.rb
+++ spec/models/master_file_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe MasterFile do
+  before do
+    @master_file = FactoryGirl.build(:master_file)
+  end
+
   it 'is invalid without a filename' do
-    mf = FactoryGirl.create(:master_file)
-    mf.should validate_presence_of(:filename)
+    FactoryGirl.build(:master_file, filename: nil).should_not be_valid
+  end
+
+  it 'is invalid with no filesize' do
+    FactoryGirl.build(:master_file, filesize: nil).should_not be_valid
+  end
+
+  it "should be destroyable, if it has no dependent records" do
+    FactoryGirl.build(:master_file).should be_destroyable
   end
 end
