5a99bb44c458984bced98df604357fbba5115325
Andrew Curley <andrew.curley@gmail.com>
2013-04-15 00:29:34 -0400

    Add support for shoulda-matchers and coveralls to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
 require 'simplecov'
 SimpleCov.start
+require 'coveralls'
+Coveralls.wear!
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+# require 'rspec/autorun'
+require 'shoulda-matchers'
+require 'shoulda/matchers/integrations/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
