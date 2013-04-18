fa9c80cb7325f6aadb6c2135e612ae87d9f21c40
Andrew Curley <andrew.curley@gmail.com>
2013-04-18 09:55:28 -0400

    Fix error with academic_statuses factory

--- spec/factories/academic_statuses.rb
+++ spec/factories/academic_statuses.rb
@@ -1,5 +1,7 @@
 FactoryGirl.modify do
-  factory :external_academic_status do
-      name 'Non-UVA'
-  end  
+  factory :academic_status do
+    factory :external_academic_status do
+        name 'Non-UVA'
+    end  
+  end
 end
